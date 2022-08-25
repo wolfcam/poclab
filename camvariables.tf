fe23c2a1700350ddeaf44012535e546532fb4979
Jean ANami <jean@br.ibm.com>
2022-08-25 19:39:41 -0300

    Initial Release3

---
+++ camvariables.tf
@@ -0,0 +1,59 @@
+{
+    "input_group_parameters":[],
+    "template_input_params": [
+        {
+                "name": "hostname",
+                "description": "Hostname da Máquina",
+                "type": "string",
+                "required": true,
+                "secured": false,
+                "label": "Hostname",
+                "hidden": false,
+                "immutable": false
+        },
+        {
+                "name": "flavor",
+                "description": "Size da VM",
+                "type": "string",
+                "required": true,
+                "secured": false,
+                "label": "Size",
+                "hidden": false,
+                "immutable": false
+        }
+	],
+"template_output_params": [
+    {
+      "name": "Name",
+      "label": "VM instance name",
+      "description": "The unique VM resource name.",
+      "type": "string",
+      "hidden": false,
+      "secured": false
+    },
+    {
+      "name": "External_IP",
+      "label": "External IP",
+      "description": "The external IP of the virtual machine.",
+      "type": "string",
+      "hidden": false,
+      "secured": false
+    },
+    {
+      "name": "Internal_IP",
+      "label": "Internal IP",
+      "description": "The internal IP of the virtual machine.",
+      "type": "string",
+      "hidden": false,
+      "secured": false
+    },
+    {
+      "name": "platform",
+      "label": "Plataforma",
+      "description": "",
+      "type": "string",
+      "hidden": false,
+      "secured": false
+    }
+]
+}
