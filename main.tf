8661530b504920fae97e4323aaadc85f8b358c54
Jean ANami <jean@br.ibm.com>
2022-08-25 19:13:14 -0300

    Initial Release

---
+++ main.tf
@@ -0,0 +1,33 @@
+resource "google_compute_instance" "novavm" {
+  name         = var.nomemaquina
+  machine_type = var.flavormaquina
+  zone         = var.maquinazona
+  boot_disk {
+    initialize_params {
+      image = var.sodisco
+    }
+  }
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
+
+output "Name" {
+  value = google_compute_instance.novavm.name
+}
+
+output "External_IP" {
+  value = google_compute_instance.novavm.network_interface[0].access_config[0].nat_ip
+}
+
+output "Internal_IP" {
+  value = google_compute_instance.novavm.network_interface[0].network_ip
+}
+
+output "Platform" {
+    value = google_compute_instance.novavm.cpu_plaform
+}
---
+++ variables.tf
@@ -0,0 +1,18 @@
+  variable "nomemaquina" {
+     default = "poclab1"
+     type = string
+  }
+  variable "flavormaquina" {
+     default = "e2-micro"
+	type = string
+  }
+ 
+ variable "maquinazona" {
+  default = "southamerica-east1-b"
+	type = string
+ }
+
+ variable "sodisco" {
+   default = "centos-cloud/centos-7"
+type = string
+ }
